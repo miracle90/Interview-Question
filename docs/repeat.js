4aa44483b116340a4eaad955aebe7401836f0647
Tony Li <394287122@qq.com>
2019-12-18 22:48:28 +0800

    Create repeat.js

---
+++ docs/repeat.js
@@ -0,0 +1,16 @@
+使用JS实现一个repeat方法，function repeat (fn, times, wait) {}，const repeatFunc = (alert, 4, 3000)，调用这个repeatFunc('Hello World')，会alert4次Hello World，每次间隔3秒
+
+
+```
+function repeat(func, times, wait) {
+  return message => {
+    func(message)
+    let timer = setInterval(() => {
+      times-- > 1 ? func(message) : clearInterval(timer);
+    }, wait);
+  };
+}
+
+const repeatFunc = repeat(console.log, 4, 3000)
+repeatFunc('Hello World')
+```
