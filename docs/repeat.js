8f49467ea823dbf1caece583eb8703b1319380d2
Tony Li <394287122@qq.com>
2019-12-18 22:48:52 +0800

    Update repeat.js

--- docs/repeat.js
+++ docs/repeat.js
@@ -1,5 +1,4 @@
-使用JS实现一个repeat方法，function repeat (fn, times, wait) {}，const repeatFunc = (alert, 4, 3000)，调用这个repeatFunc('Hello World')，会alert4次Hello World，每次间隔3秒
-
+// 使用JS实现一个repeat方法，function repeat (fn, times, wait) {}，const repeatFunc = (alert, 4, 3000)，调用这个repeatFunc('Hello World')，会alert4次Hello World，每次间隔3秒
 
 ```
 function repeat(func, times, wait) {
